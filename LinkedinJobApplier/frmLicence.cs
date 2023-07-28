4507694b5e3fa1f3421132b2c67fe3cef3b743e9
alpersarac <alper.sarac42@gmail.com>
2023-07-28 22:06:19 +0300

    time for all

--- LinkedinJobApplier/frmLicence.cs
+++ LinkedinJobApplier/frmLicence.cs
@@ -28,41 +28,51 @@ namespace LinkedinJobApplier
         {
             try
             {
-                string licenseKey = tbxLicence.Text.Replace(" ","");
+                string licenseKey = tbxLicence.Text.Replace(" ", "");
                 string email = tbxEmail.Text;
                 bool isConnectionOK = false;
-                if (string.IsNullOrEmpty(licenseKey))
+                DateTime? currentDateTime = new DateTime();
+
+                currentDateTime = WordTimerManager.GetCurrentDateTime();
+                if (currentDateTime == null)
                 {
-                    MessageBox.Show("Enter a licence key", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                    return;
-                }
-                if (!Checker.CheckEmail(tbxEmail.Text)|| string.IsNullOrEmpty(email))
-                {
-                    MessageBox.Show("Enter an valid email", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                    return;
-                }
-                LicenceTable parsedLicenseTable = LicenseKeyManager.ParseLicenseKey(licenseKey,ref isConnectionOK);
-                if (parsedLicenseTable.expirydate < DateTime.Now)
-                {
-                    MessageBox.Show("Licence is expired", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
-                }
-                else if (parsedLicenseTable!=null&&!parsedLicenseTable.isonline)
-                {
-                    // Encrypt and save the license key
-                    LicenseKeyManager.SaveLicenseKey(licenseKey);
-                   
-                    LicenseKeyManager.SetMacAddress(parsedLicenseTable, email, NetworkHelper.GetMacAddress());
-                    MessageBox.Show("Registration is successfully completed", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                    isRegistered = true;
-                    this.Hide();
-                    _frmMainObj.SetTabsPages(parsedLicenseTable.isinfoextrator);
-                    _frmMainObj.Show();
-                    //----show frmmain
+                    MessageBox.Show("Unable connect to internet, please check your internet connection", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
                 else
                 {
-                    MessageBox.Show("Registration key is wrong", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                    isRegistered = false;
+                    if (string.IsNullOrEmpty(licenseKey))
+                    {
+                        MessageBox.Show("Enter a licence key", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        return;
+                    }
+                    if (!Checker.CheckEmail(tbxEmail.Text) || string.IsNullOrEmpty(email))
+                    {
+                        MessageBox.Show("Enter an valid email", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        return;
+                    }
+                    LicenceTable parsedLicenseTable = LicenseKeyManager.ParseLicenseKey(licenseKey, ref isConnectionOK);
+                    if (parsedLicenseTable.expirydate < DateTime.Now)
+                    {
+                        MessageBox.Show("Licence is expired", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    }
+                    else if (parsedLicenseTable != null && !parsedLicenseTable.isonline)
+                    {
+                        // Encrypt and save the license key
+                        LicenseKeyManager.SaveLicenseKey(licenseKey);
+
+                        LicenseKeyManager.SetMacAddress(parsedLicenseTable, email, NetworkHelper.GetMacAddress());
+                        MessageBox.Show("Registration is successfully completed", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        isRegistered = true;
+                        this.Hide();
+                        _frmMainObj.SetTabsPages(parsedLicenseTable.isinfoextrator);
+                        _frmMainObj.Show();
+                        //----show frmmain
+                    }
+                    else
+                    {
+                        MessageBox.Show("Registration key is wrong", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        isRegistered = false;
+                    }
                 }
 
             }
@@ -94,7 +104,7 @@ namespace LinkedinJobApplier
                     e.Cancel = true;
                 }
             }
-            
+
         }
     }
 }
